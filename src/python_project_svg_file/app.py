a72ea00c4f9d1571a031eb15981c2aa7606ef60a
Kélian PONS <kelian.pons@centrale.centralelille.fr>
2025-10-03 11:17:36 +0200

    Creating the marimo notebook

---
+++ src/python_project_svg_file/app.py
@@ -0,0 +1,25 @@
+import marimo
+
+__generated_with = "0.16.5"
+app = marimo.App(width="medium")
+
+
+@app.cell
+def _():
+    import marimo as mo
+    import numpy as np
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+@app.cell
+def _():
+    return
+
+
+if __name__ == "__main__":
+    app.run()

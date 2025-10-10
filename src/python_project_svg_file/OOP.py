c14eb352a47d5a66e540f6e19ee90d8b6e13708a
Kélian PONS <kelian.pons@centrale.centralelille.fr>
2025-10-10 12:06:57 +0200

    Adding the showing_map func to check the mapping

--- src/python_project_svg_file/OOP.py
+++ src/python_project_svg_file/OOP.py
@@ -9,16 +9,19 @@ class HexTransform:
         self.xmax = self.input_image.size[0]
         self.ymax = self.input_image.size[1]
         self.img_ratio = self.xmax/self.ymax
+        self.input_maping = None
+        self.input_hex_size = None
         pass
 
     def __str__(self):
         return f"Hello"
 
     def map(self, hex_per_col):
-
+        
         vert_spacing = self.ymax / (hex_per_col + 0.5)
         off_set = 0.5 * vert_spacing
         hex_size = vert_spacing /(np.sqrt(3))
+        self.input_hex_size = hex_size
         horiz_spacing = 3 / 2 * hex_size
 
         hex_per_row = int(self.xmax // horiz_spacing) + 1 
@@ -39,11 +42,24 @@ class HexTransform:
                 hex_mapping[i, j, 0] = j * horiz_spacing
                 hex_mapping[i, j, 1] = i * vert_spacing + off_set
 
+        self.input_maping = hex_mapping
+
         return hex_mapping
     
     def show_mapping(self):
-        pass
-
+        m, n = self.input_maping.shape[0], self.input_maping.shape[1]
+        image = self.input_image.copy()
+        draw = ImageDraw.Draw(image)
+        for j in range(m):
+            for i in range(n):
+                if (
+                    self.input_maping[j, i, 0] > self.input_hex_size
+                    and self.input_maping[j, i, 1] > self.input_hex_size
+                    and self.input_maping[j, i, 0] < self.xmax - self.input_hex_size
+                    and self.input_maping[j, i, 1] < self.ymax - self.input_hex_size
+                ):
+                    draw.circle((self.input_maping[j, i, 0], self.input_maping[j, i, 1]), radius=self.input_hex_size, outline=(0, 255, 0, 0))
+        image.show()
 
     def build_svg(self,output_path):
         pass
@@ -56,4 +72,5 @@ if __name__ == "__main__":
     # print('This is run from OOP.py file')
     hex_map = HexTransform()
     print(hex_map)
-    
+    hex_map.map(22)
+    hex_map.show_mapping()
